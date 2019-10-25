e8026050f51fe63acec4f663891a6858a1b77a95
Kjell Arne Hellum <kjell@justify.no>
2019-10-25 02:58:25 +0200

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Blezzenger.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static readonly string SendMessageMethod = "_sendMessage";
+        public void SendMessage(string sender, string message)
+        {
+            Clients.Others.SendAsync(SendMessageMethod, sender, message);
+        }
+    }
+}
---
+++ Models/MessageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Blezzenger.Models
+{
+    public class MessageModel
+    {
+        public DateTimeOffset Timestamp { get; private set; }
+        public string Message { get; private set; }
+        public string Sender { get; private set; }
+        public bool IsSystemMessage { get; private set; }
+        public bool IsSentByMe { get; private set; }
+        public bool IsConsecutiveFromSender { get; private set; }
+        public MessageModel(string message, string sender, bool sentByMe, bool isSystemMessage = false, bool isConsecutiveFromSender = false)
+        {
+            Timestamp = DateTimeOffset.UtcNow;
+            Message = message;
+            Sender = sender;
+            IsSentByMe = sentByMe;
+            IsSystemMessage = isSystemMessage;
+            IsConsecutiveFromSender = isConsecutiveFromSender;
+        }
+    }
+}
