95506d6ee613d48d9d20ea0d4e64880ea1f79922
Kjell Arne Hellum <kjell@justify.no>
2019-10-25 03:38:17 +0200

    Pimps it up

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -5,9 +5,21 @@ namespace Blezzenger.Hubs
     public class ChatHub : Hub
     {
         public static readonly string SendMessageMethod = "_sendMessage";
+        public static readonly string SendHelloMethod = "_sendHello";
+        public static readonly string SendHelloBackMethod = "_sendHelloBack";
         public void SendMessage(string sender, string message)
         {
             Clients.Others.SendAsync(SendMessageMethod, sender, message);
+        }
+
+        public void SendHello(string joiner)
+        {
+            Clients.Others.SendAsync(SendHelloMethod, joiner);
+        }
+
+        public void SendHelloBack(string me)
+        {
+            Clients.Others.SendAsync(SendHelloBackMethod, me);
         }
     }
 }
